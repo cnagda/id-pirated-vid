9ac45d9a1d170deed203bc7c4c9808cc833ee2b6
Andrei <aoblebea@ucdavis.edu>
2020-01-27 20:39:15 -0800

    I love resolving dependency issues

--- main.cxx
+++ main.cxx
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
+#include <opencv2/features2d.hpp>
 
 using namespace cv;
 using namespace std;
@@ -14,11 +15,23 @@ int main(int argc, char** argv )
 
     namedWindow("Display window", WINDOW_AUTOSIZE );// Create a window for display.
 
+    SiftFeatureDetector detector;
+    vector<cv::KeyPoint> keypoints;
+    detector.detect(image, keypoints);
+
+    // Add results to image and save.
+    Mat output;
+
 
     VideoCapture cap(argv[1], CAP_ANY);
     while(cap.read(image)) {
-        cout << "size: " << image.total() << endl;
-        imshow("Display window", image);
+	detector.detect(image, keypoints);
+	drawKeypoints(image, keypoints, output);
+
+
+        cout << "size: " << output.total() << endl;
+        imshow("Display window", output);
+	waitKey(0);
     }
     return 0;
 }
