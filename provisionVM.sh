dc085264ddb383f307f548fca029ac7d293bbc5e
klanmiko <eternity01033@gmail.com>
2020-01-28 15:08:31 -0800

    update

--- provisionVM.sh
+++ provisionVM.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 apt-get update
-apt-get install -y build-essential cmake ffmpeg
+apt-get install -y build-essential cmake ffmpeg libgtk-3-dev
 mkdir /OpenCV
 cd /OpenCV
 git clone --depth=1 --branch 4.2.0 https://github.com/opencv/opencv.git
@@ -9,7 +9,7 @@ mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local \
  -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules -D WITH_FFMPEG=ON -D WITH_OPENMP=ON \
- -DOPENCV_ENABLE_NONFREE=ON -D BUILD_EXAMPLES=OFF -D BUILD_opencv_apps=OFF -D BUILD_DOCS=OFF \
+ -DOPENCV_ENABLE_NONFREE=ON -D BUILD_EXAMPLES=OFF -WITH_GTK=ON -D BUILD_opencv_apps=OFF -D BUILD_DOCS=OFF \
 -D BUILD_PERF_TESTS=OFF -D BUILD_TESTS=OFF ../opencv
 make -j8
 make install
