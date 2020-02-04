af26c01d7c8ce051f0569b4987e403cca9734407
klanmiko <eternity01033@gmail.com>
2020-02-04 12:36:35 -0800

    database time

--- include/database.hpp
+++ include/database.hpp
@@ -4,6 +4,13 @@
 #include <vector>
 #include <memory>
 #include <string>
+#include <opencv2/opencv.hpp>
+#include <functional>
+
+void SIFTwrite(const std::string& filename, const cv::Mat& mat, const std::vector<cv::KeyPoint>& keyPoints);
+std::pair<cv::Mat, std::vector<cv::KeyPoint>> SIFTread(const std::string& filename);
+std::string getAlphas(std::string input);
+void createFolder(std::string folder_name);
 
 class IVideo {
 public:
@@ -21,13 +28,13 @@ public:
 
 class IDatabase {
 public:
-    virtual std::unique_ptr<IVideo> addVideo(const std::string& filepath) = 0;
+    virtual std::unique_ptr<IVideo> addVideo(const std::string& filepath, std::function<void(cv::Mat, Frame)> callback = nullptr) = 0;
     virtual std::unique_ptr<IVideo> loadVideo(const std::string& filepath) = 0;
 };
 
 class FileDatabase : public IDatabase {
 public:
-    std::unique_ptr<IVideo> addVideo(const std::string& filepath);
+    std::unique_ptr<IVideo> addVideo(const std::string& filepath, std::function<void(cv::Mat, Frame)> callback = nullptr);
     std::unique_ptr<IVideo> loadVideo(const std::string& filepath);
 };
 
