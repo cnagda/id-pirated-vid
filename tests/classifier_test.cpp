82012c185391ef6dc12cb3b8b76f45df61e83b0d
Kaelan Mikowicz <eternity01033@gmail.com>
2020-02-19 00:21:23 -0800

    classifier class

---
+++ tests/classifier_test.cpp
@@ -0,0 +1,42 @@
+#include "gtest/gtest.h"
+#include "classifier.hpp"
+
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <iostream>
+
+template<typename... Args> bool alwaysTrueClassifier(Args&&...) { return true; }
+
+TEST(Classifier, getAccuracy) {
+    std::vector<labelled_test<int>> tests;
+    std::generate_n(std::back_inserter(tests), 100, [i = 0, val = false]() mutable {
+        auto ret = std::make_pair(val, i++);
+        val = !val;
+        return ret;
+    });
+    
+    {
+        auto&& results = BinaryResults::runClassifier(tests.begin(), tests.end(), alwaysTrueClassifier<int>);
+        auto&& matrix = results.getConfusionMatrix();
+        std::cout << "tp: " << matrix.tp 
+                    << "tn: " << matrix.tn 
+                    << "fp: " << matrix.fp 
+                    << "fn: " << matrix.fn << std::endl;
+        ASSERT_EQ(0.5, getAccuracy(matrix));
+    }
+
+    std::generate_n(tests.begin(), 100, []{ return std::make_pair(false, 0); });
+
+    {
+        auto&& results = BinaryResults::runClassifier(tests.begin(), tests.end(), alwaysTrueClassifier<int>);
+        ASSERT_EQ(0, getAccuracy(results.getConfusionMatrix()));
+    }
+
+    std::generate_n(tests.begin(), 100, []{ return std::make_pair(true, 0); });
+
+    {
+        auto&& results = BinaryResults::runClassifier(tests.begin(), tests.end(), alwaysTrueClassifier<int>);
+        ASSERT_EQ(1, getAccuracy(results.getConfusionMatrix()));
+    }
+}
