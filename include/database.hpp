85f06bbfebbccb094565e87b432dbf34b85990ec
klanmiko <eternity01033@gmail.com>
2020-02-26 10:45:22 -0800

    database improvements

--- include/database.hpp
+++ include/database.hpp
@@ -27,7 +27,21 @@ public:
 
     virtual size_type frameCount() = 0;
     virtual std::vector<Frame>& frames() & = 0;
+    virtual const std::vector<IScene>& getScenes() & = 0;
     virtual ~IVideo() = default;
+};
+
+class IScene {
+public:
+    IScene(const std::string& key) : key(key) {};
+    virtual ~IScene() = default;
+    const std::string key;
+
+    template<typename Extractor> virtual auto getDescriptor(Extractor&& ext) {
+        return ext(getFrames());
+    }
+
+    virtual vector<Frame>& getFrames() & = 0;
 };
 
 class SIFTVideo : public IVideo {
@@ -41,12 +55,43 @@ public:
     size_type frameCount() override { return SIFTFrames.size(); };
 };
 
+template<typename T>
+class ICursor {
+public:
+    ICursor& advance() & = 0;
+    operator bool()() = 0;
+    const T& getValue() & const = 0;
+};
+
 class IDatabase {
 public:
-    virtual std::unique_ptr<IVideo> addVideo(const std::string& filepath, std::function<void(cv::Mat, Frame)> callback = nullptr) = 0;
-    virtual std::unique_ptr<IVideo> loadVideo(const std::string& filepath) const = 0;
-    virtual std::vector<std::string> listVideos() const = 0;
+    virtual std::unique_ptr<IVideo> saveVideo(const IVideo& video) = 0;
+    virtual Cursor<IVideo> loadVideo(const std::string& key = "") const = 0;
+    template<typename Vocab> virtual saveVocab(Vocab&& vocab, const std::string& key) = 0;
+    template<typename Vocab> virtual Vocab loadVocab(const std::string& key) = 0;
     virtual ~IDatabase() = default;
+};
+
+class IVideoLoadStrategy {
+public:
+    virtual std::unique_ptr<IVideo> operator()(const std::string& findKey) const = 0;
+    virtual ~IVideoLoadStrategy() = default;
+};
+
+class IVideoStorageStrategy {
+public:
+    virtual IVideo& operator()(IVideo& video, IDatabase& database) const = 0;
+    virtual ~IVideoStorageStrategy() = default;
+};
+
+class SubdirSearchStrategy : IVideoLoadStrategy {
+public:
+    std::unique_ptr<IVideo> operator()(const std::string& findKey) const;
+};
+
+class EagerStorageStrategy : IVideoStorageStrategy {
+public:
+    IVideo& operator()(IVideo& video, IDatabase& database) const ;
 };
 
 class FileDatabase : public IDatabase {
