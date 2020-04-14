af8ea80acf4a4bc6471484fd5c6cf24092b4448f
Chandni Nagda <chandninagda@gmail.com>
2020-04-13 19:20:54 -0700

    added projection/warp attack

--- python/pirater.py
+++ python/pirater.py
@@ -18,9 +18,20 @@ down_multiplier_large = 0.5
 static_path = "../assets/static.mp4"
 snow_path = "../assets/snow.mp4"
 
+def trapzWarp(frame,cx,cy,ismask=False):
+    """ Complicated function (will be latex packaged as a fx) """
+    Y,X = frame.shape[:2]
+    src = np.array([[0,0],[X,0],[X,Y],[0,Y]])
+    dst = np.array([[cx*X,cy*Y],[(1-cx)*X,cy*Y],[X,Y],[0,Y]])
+    tform = tf.ProjectiveTransform()
+    tform.estimate(src,dst)
+    im = tf.warp(frame, tform.inverse, output_shape=(Y,X))
+    return im if ismask else (im*255).astype('uint8')
+
+
 class Attack:
     def frame(self, up):
-        print(f"frame up={up}")
+        # print(f"frame up={up}")
         # newvid = self.speed(up)
         if up is True:
             frame_rate_multiplier = up_multiplier_large
@@ -30,8 +41,9 @@ class Attack:
 
         return self.vid
 
+
     def scale(self, up, noise=False):
-        print(f"scale in={up}, noise={noise}")
+        # print(f"scale in={up}, noise={noise}")
 
         current_w = self.vid.w
         current_h = self.vid.h
@@ -52,12 +64,14 @@ class Attack:
                     opacity = 0.0
                 )
                 # TODO: chandni\
-                static_clip = VideoFileClip(static_path).fx(vfx.loop, duration=newvid.duration)
+                static_clip = VideoFileClip(static_path).fx(vfx.loop,
+                    duration=newvid.duration)
                     # static_clip = static_clip.crop(
                     #     width = current_w,
                     #     height = current_h
                     # )
-                newvid = CompositeVideoClip([static_clip, newvid], size=(current_w,current_h))
+                newvid = CompositeVideoClip([static_clip, newvid],
+                    size=(current_w,current_h))
             else:
                 newvid = newvid.margin(
                     left = math.ceil(abs(current_w - newvid.w) / 2),
@@ -68,8 +82,9 @@ class Attack:
 
         return newvid
 
+
     def recolor(self, grey=False, dark=False):
-        print(f"recolor grey={grey} dark={dark}")
+        # print(f"recolor grey={grey} dark={dark}")
         if grey is True:
             newvid = self.vid.fx(vfx.blackwhite)
         else:
@@ -77,8 +92,9 @@ class Attack:
 
         return newvid
 
+
     def rotate(self, deg):
-        print(f"rotate deg={deg}")
+        # print(f"rotate deg={deg}")
         current_h = self.vid.h
         current_w = self.vid.w
         newvid = self.vid.rotate(deg)
@@ -92,74 +108,79 @@ class Attack:
             )
         return newvid
 
+
     def speed(self, up):
-        print(f"speed up={up}")
+        # print(f"speed up={up}")
         if up == True:
             multiplier = up_multiplier_large
         else:
             multiplier = down_multiplier_large
         return self.vid.speedx(factor=multiplier)
 
-    # https://zulko.github.io/moviepy/examples/star_worms.html
-    def bottomWarp(self, pic):
-        Y,X = pic.shape[:2]
-        scale_factor = 0.1
-        src = np.array([[0,0],[X,0],[X,Y],[0,Y]])
-        dst = np.array([[0,0],[X,0],[X - scale_factor * X,Y],[0 + scale_factor * X,Y]])
-        tform = tf.ProjectiveTransform()
-        tform.estimate(src ,dst)
-        im = tf.warp(pic, tform)
-        return im
 
     def projection(self):
-        warp_im = lambda pic : self.bottomWarp(pic)
-        modified_clip= self.vid.fl_image(warp_im)
         print("projection")
+        fl_im = lambda frame : trapzWarp(frame,0.1,0.1)
+        warped_vid = self.vid.fl_image(fl_im)
+        return warped_vid
 
-        return modified_clip
 
     def exact_match(self):
         print("exact_match")
         return self.vid
 
+
     def snowflakes(self):
         print("snowflakes")
-        snowflakes = VideoFileClip(snow_path).fx(vfx.loop, duration=self.vid.duration)
+        snowflakes = VideoFileClip(snow_path).fx(vfx.loop,
+            duration=self.vid.duration)
         snowflakes = snowflakes.resize(newsize=(self.vid.w, self.vid.h))
         snowflakes = snowflakes.fx(vfx.mask_color, color=[0,0,255], thr=850)
-        newvid = CompositeVideoClip([self.vid, snowflakes], size=(self.vid.w,self.vid.h))
+        newvid = CompositeVideoClip([self.vid, snowflakes],
+            size=(self.vid.w,self.vid.h))
         return newvid
+
 
     def scale_up(self):
         return self.scale(up=True)
 
+
     def scale_down_black(self):
         return self.scale(up=False,noise=False)
+
 
     def scale_down_noise(self):
         return self.scale(up=False, noise=True)
 
+
     def frame_rate_up(self):
         return self.frame(up=True)
+
 
     def frame_rate_down(self):
         return self.frame(up=False)
 
+
     def recolor_grey(self):
         return self.recolor(grey=True)
+
 
     def recolor_dark(self):
         return self.recolor(dark=True)
 
+
     def rotate_90(self):
         return self.rotate(deg=90)
+
 
     def rotate_180(self):
         return self.rotate(deg=180)
 
+
     def mirror(self):
         print("mirror")
-        return self.vid.fx(vfx.mirror_x)
+        return
+        self.vid.fx(vfx.mirror_x)
 
     def pic_in_pic(self):
         print("pic_in_pic")
@@ -171,11 +192,14 @@ class Attack:
         )
         return newvid
 
+
     def speed_up(self):
         return self.speed(up=True)
 
+
     def speed_down(self):
         return self.speed(up=False)
+
 
     def insert_clip(self, video):
         nonpirated_length = math.floor(self.base_video.duration)
@@ -187,6 +211,7 @@ class Attack:
         clip2 = video.subclip(t_start=start_clip, t_end=start_clip + clip_length)
         clip3 = self.base_video.subclip(t_start=6, t_end=split_point)
         return concatenate_videoclips([clip1,clip2,clip3])
+
 
     def generate_all(self):
         for attack_function in self.attack_functions:
@@ -204,22 +229,18 @@ class Attack:
             del self.vid.reader
             del self.vid
 
+
     def __init__(self, vidpath, destdir, basepath):
         self.vidpath = vidpath
-        # self.vid = VideoFileClip(vidpath)
         self.destdir = destdir
         self.base_video = VideoFileClip(basepath)
-
-        # not working:
-        # self.projection
-
         self.attack_functions = [
-            # self.projection,
+            self.projection,
             # self.exact_match,
             # self.snowflakes,
             # self.scale_up,
             # self.scale_down_black,
-            self.scale_down_noise,
+            # self.scale_down_noise,
             # self.frame_rate_up,
             # self.frame_rate_down,
             # self.recolor_grey,
@@ -232,12 +253,6 @@ class Attack:
             # self.speed_down
         ]
 
-
-
-# def generate_and_save_all_attacks(vidpath, destdir):
-#     for attack_function in attack_functions:
-#         video = attack_function(vidpath)
-#         video.write_videofile(destdir, audio=False)
 
 def main():
     parser = argparse.ArgumentParser(
@@ -264,6 +279,7 @@ def main():
     for vidpath in fullvids:
         Attacker = Attack(vidpath, args.destdir, args.extravid)
         Attacker.generate_all()
+
 
 if __name__ == '__main__':
     main()
