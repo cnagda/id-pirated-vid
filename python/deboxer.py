4687ac54047fc88bbd978e69a15ffd1edfac54f8
The-Boogeyman <aoblebea@ucdavis.edu>
2020-05-04 19:39:11 -0700

    segmenter can preprocess an image to make line detection work better

--- python/deboxer.py
+++ python/deboxer.py
@@ -2,6 +2,7 @@ import cv2
 import sys
 import numpy as np
 import math
+from matplotlib import pyplot as plt 
 
 print("Args: " +  str(sys.argv))
 if(len(sys.argv) != 2):
@@ -9,6 +10,26 @@ if(len(sys.argv) != 2):
 print("Hi")
 
 img = cv2.imread(sys.argv[1], cv2.IMREAD_GRAYSCALE)
+
+#image = cv2.imread(sys.argv[1])
+#mask = np.zeros(image.shape[:2], np.uint8) 
+   
+#backgroundModel = np.zeros((1, 65), np.float64) 
+#foregroundModel = np.zeros((1, 65), np.float64) 
+   
+#rectangle = (20, 100, 150, 150) 
+   
+#cv2.grabCut(image, mask, rectangle,   
+#            backgroundModel, foregroundModel, 
+#            3, cv2.GC_INIT_WITH_RECT) 
+   
+#mask2 = np.where((mask == 2)|(mask == 0), 0, 1).astype('uint8') 
+   
+#image = image * mask2[:, :, np.newaxis] 
+   
+#cv2.imshow('cut', image) 
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
 
 cv2.imshow('image',img)
 cv2.waitKey(0)
@@ -19,20 +40,21 @@ cv2.imshow('image',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-edges = cv2.Canny(img,300,400)
-#edges = cv2.Canny(img,100,200)
+#edges = cv2.Canny(img,300,400)
+edges = cv2.Canny(img,100,200)
+#edges = cv2.Canny(img,100,200, 7)
 
 cv2.imshow('edges', edges)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-lines = cv2.HoughLines(edges,1,np.pi/180,200)
+lines = cv2.HoughLines(edges,1,np.pi/180,100)
 
 print(lines.shape)
 print(lines)
 
 index = 0
-maxlines = 10
+maxlines = 1000
 intervals = 180
 slotwidth = 360 // intervals
 ortho_dist = 90 // slotwidth
@@ -63,6 +85,8 @@ if lines is not None:
 
         slot = round(angle / slotwidth) % intervals
         organized_lines[slot].append((x0, y0, b, a))
+
+#print(organized_lines)
 
 for i in range(0, len(organized_lines)):
     next = (i + ortho_dist) % intervals
---
+++ python/segmenter.py
@@ -0,0 +1,36 @@
+from skimage import data
+#import scikit-image
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import skimage.segmentation as seg
+import cv2
+import skimage.draw as draw
+import skimage.color as color
+
+print("Args: " +  str(sys.argv))
+if(len(sys.argv) != 2):
+    quit()
+
+image = cv2.imread(sys.argv[1])
+
+image_felzenszwalb = seg.felzenszwalb(image)
+image_felzenszwalb = image_felzenszwalb.astype(np.uint8)
+image_slic = seg.slic(image,n_segments=155)
+#image_slic = seg.slic(image,n_segments=50)
+image_slic = image_slic.astype(np.uint8)
+#image_show(image_felzenszwalb);
+cv2.imshow('img', image_felzenszwalb)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imshow('img', image_slic)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+colored = color.label2rgb(image_slic, image, kind='avg')
+colored = colored.astype(np.uint8)
+cv2.imshow('img', colored)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.imwrite("../blobbified.png", colored)
