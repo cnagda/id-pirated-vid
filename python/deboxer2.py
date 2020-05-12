e6eddb72bb5634e1485e04c9b8491776a1b60c32
The-Boogeyman <aoblebea@ucdavis.edu>
2020-05-11 20:15:43 -0700

    Basic box detection for boxes in corner

--- python/deboxer2.py
+++ python/deboxer2.py
@@ -13,14 +13,18 @@ if(len(sys.argv) != 2):
     quit()
 
 cap = cv2.VideoCapture(sys.argv[1])
+numframes = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
 counter = -1
-speedinator = 100
+speedinator = 10
 
 debug = False
 
 ys = []
 xs = []
+
+bestx = -1
+besty = -1
 
 while(cap.isOpened()):
 
@@ -28,7 +32,7 @@ while(cap.isOpened()):
 
     ret, image = cap.read()
 
-    print("Frame " + str(counter))
+    print("Frame " + str(counter) + "/" + str(numframes))
 
     counter += 1
     if(counter % speedinator != 0):
@@ -38,12 +42,13 @@ while(cap.isOpened()):
         break
 
     #image = cv2.resize(image, (1920, 1080))
+    # FIXME swap
     width, height = image.shape[:2]
     linesize = int(min(width, height)/4)
 
 
-    #image_slic = seg.slic(image,n_segments=155)
-    image_slic = seg.slic(image,n_segments=30)
+    image_slic = seg.slic(image,n_segments=155)
+    #image_slic = seg.slic(image,n_segments=30)
     image_slic = image_slic.astype(np.uint8)
 
     colored = color.label2rgb(image_slic, image, kind='avg')
@@ -138,6 +143,7 @@ print(len(ys))
 cap = cv2.VideoCapture(sys.argv[1])
 ret, image = cap.read()
 
+# FIXME swap
 width, height = image.shape[:2]
 
 xs = np.asarray(xs, dtype=np.float32)
@@ -192,13 +198,102 @@ for i in range(0, cy.cluster_centers_.shape[0]):
 pt1 = (mx, 0)
 pt2 = (mx, height - 1)
 cv2.line(image, pt1, pt2, (255,0,0), 3, cv2.LINE_AA)
+bestx = int(mx[0])
 
 pt1 = (0, my)
 pt2 = (width - 1, my)
 cv2.line(image, pt1, pt2, (255,0,0), 3, cv2.LINE_AA)
+besty = int(my[0])
 
 
 cv2.imshow('image',image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+print("================================================================ END LINE DETECTION ==============================================================================")
+
+diffcounts = [0, 0, 0, 0]
+counter = -1
+
+while(cap.isOpened()):
+
+    #image = cv2.imread(sys.argv[1])
+
+    ret, image = cap.read()
+
+    print("Frame " + str(counter) + "/" + str(numframes))
+
+    counter += 1
+    if(counter % speedinator != 0):
+        continue
+
+    if(image is None):
+        break
+
+    #image = cv2.resize(image, (1920, 1080))
+    # FIXME swap
+    width, height = image.shape[:2]
+
+    subimgs = [image[0:bestx, 0:besty], image[bestx:width, 0:besty], image[0:bestx, besty:height], image[bestx:width, besty:height]]
+
+    diffs = [0, 0, 0, 0]
+
+    # histogram stuff
+    h_bins = 50
+    s_bins = 60
+    histSize = [h_bins, s_bins]
+
+    h_ranges = [0, 180]
+    s_ranges = [0, 256]
+    ranges = h_ranges + s_ranges
+    channels = [0, 1]
+
+    def ashist(img):
+        csv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        hist = cv2.calcHist([csv], channels, None, histSize, ranges, accumulate=False)
+        cv2.normalize(hist, hist, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
+        return hist
+
+    hists = list(map(ashist, subimgs))
+
+    for i in range(4):
+        for j in range(4):
+            if(i == j):
+                continue
+            diffs[i] += 1 - cv2.compareHist(hists[i], hists[j], 0) # correlation
+
+    diffcounts[diffs.index(max(diffs))] += 1
+
+print(diffcounts)
+
+outbox = diffcounts.index(max(diffcounts))
+
+cap = cv2.VideoCapture(sys.argv[1])
+ret, image = cap.read()
+#width, height = image.shape[:2]
+height, width = image.shape[:2]
+
+xl, yl, xh, yh = (0, 0, 0, 0)
+
+if(outbox == 0):
+    xl, yl, xh, yh = (0, 0, bestx, besty)
+
+if(outbox == 1):
+    xl, yl, xh, yh = (bestx, 0, width-1, besty)
+    
+if(outbox == 2):
+    xl, yl, xh, yh = (0, besty, bestx, height-1)
+    
+if(outbox == 3):
+    xl, yl, xh, yh = (bestx, besty, width-1, height-1)
+    
+
+print(width)
+print(height)
+print(xh)
+print(yh)
+cv2.rectangle(image, (xl, yl), (xh, yh), (255,0,0), -1)
+
+cv2.imshow('image',image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
