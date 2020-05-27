00bc5ede9aabe18b622a1be8615c3852442796ea
Emma Kristovich <emkristovich@gmail.com>
2020-05-27 09:20:20 -0700

    files save in results now

--- python/experiment.py
+++ python/experiment.py
@@ -1,104 +1,11 @@
 from skimage import data
+import argparse
 import numpy as np
 import sys
 import cv2
-# from moviepy.editor import *
+import os
 from moviepy.video.io.VideoFileClip import *
 from moviepy.video.fx.crop import crop
-
-print("Args: " +  str(sys.argv))
-if(len(sys.argv) != 2):
-    quit()
-
-cap = cv2.VideoCapture(sys.argv[1])
-numframes = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-
-counter = -1
-speedinator = 10
-
-thresh = 10
-val = -30
-
-allimages = []
-first = True
-total_image = 0
-
-debug = False
-
-while(cap.isOpened()):
-
-    counter += 1
-
-    #image = cv2.imread(sys.argv[1])
-
-    ret, image = cap.read()
-
-
-    print("Frame " + str(counter) + "/" + str(numframes))
-
-    if(counter % speedinator != 0):
-        continue
-
-    if(image is None):
-        break
-
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    laplacian = cv2.Laplacian(image, cv2.CV_8U)
-    laplacian = laplacian.astype(int)
-    laplacian[laplacian < thresh] = val
-
-    if(first):
-        total_image = laplacian
-        first = False
-    else:
-        total_image += laplacian
-
-
-    #ret, laplacian = cv2.threshold(laplacian,127,255,cv2.THRESH_BINARY)
-
-    allimages.append(laplacian)
-
-    #cv2.imshow('img', sobel_8u)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
-
-#print(total_image)
-
-#av = np.mean(allimages, axis=0)
-total_image = total_image.astype(float)
-total_image /= counter
-
-#print(total_image)
-
-#print(allimages[0].shape)
-#print(av.shape)
-total_image = np.clip(total_image, 0, 255)
-
-total_image *= (255 / total_image.max())
-
-total_image = total_image.astype(np.uint8)
-
-#print(total_image)
-
-if(debug):
-    cv2.imshow('img', total_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-total_image = cv2.threshold(total_image, 20, 255, cv2.THRESH_BINARY)[1]
-#edges = cv2.Canny(total_image,0,0)
-edges = total_image
-
-if(debug):
-    cv2.imshow('img', total_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-height, width = total_image.shape[:2]
-linesize = int(min(width, height)/8)
-
-padding = int(min(width, height)/8)
 
 
 def maxsubarray(arr):
@@ -144,121 +51,257 @@ def maxsubarray(arr):
         #return max(dp)
     return (start, end)
 
-flat = np.sum(edges, axis = 0)
-flat[0] = 0
-s = flat.shape[0]
-flat[s - 1] = 0
-print("Flat shape: " + str(s))
-#print(flat)
-
-#plt.plot(range(s), flat)
-#plt.show()
-
-print("Maxcol: " + str(flat.argmax()) + "/" + str(s))
-#print(flat)
-#print("==========================================================================================================")
-
-edges = edges.astype(int)
-edges[edges == 0] = -255
-
-maxc = -1
-maxcs = -1
-maxce = -1
-
-maxr = -1
-maxrs = -1
-maxre = 0-1
-
-for i in range(3, width - 3):
-    col = edges[:, i]
-    start, end = maxsubarray(col)
-
-    if(end - start > maxce - maxcs):
-        maxce = end
-        maxcs = start
-        maxc = i
-
-    #print("Col " + str(i) + "/" + str(width) + ": " + str(start) + " -> " + str(end))
-
-for i in range(3, height - 3):
-    row = edges[i, :]
-    start, end = maxsubarray(row)
-    
-    if(end - start > maxce - maxcs):
-        maxre = end
-        maxrs = start
-        maxr = i
+def main():
+    python_folder = os.path.abspath(os.path.dirname(__file__))
+    root_dir = os.path.normpath(os.path.join(python_folder, ".."))
+    results_dir = os.path.join(root_dir, "results")
+    if not os.path.exists(results_dir):
+        os.makedirs(results_dir)
 
 
-if(maxce - maxcs < linesize or maxre - maxrs < linesize):
-    print("Did not detect two lines")
-    quit()
+    outer_path = os.path.join(results_dir, "outervideo.mp4")
+    box_path = os.path.join(results_dir, "boxvideo.mp4")
 
-cap = cv2.VideoCapture(sys.argv[1])
-ret, image = cap.read()
+    # deleting files if they already exist because query will check if exists
+    # before running on both videos
+    if os.path.exists(outer_path):
+        os.remove(outer_path)
+    if os.path.exists(box_path):
+        os.remove(box_path)
 
 
-cv2.line(image, (maxc, maxcs), (maxc, maxce), (0,0,255), 3, cv2.LINE_AA)
-cv2.line(image, (maxrs, maxr), (maxre, maxr), (0,0,255), 3, cv2.LINE_AA)
 
-if(debug):
-    cv2.imshow('img', image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # print("Args: " +  str(sys.argv))
+    if(len(sys.argv) != 2):
+        quit()
 
-wig = 2 # wiggle room for lines to overlap
+    parser = argparse.ArgumentParser(
+        description='Find picture in picture and save separate files'
+    )
 
-right = False
-left = False
-up = False
-down = False
+    parser.add_argument(
+        'srcpath',
+        type=str,
+        help='path to video with potential picture in picture'
+    )
 
-if(maxrs >= maxc - wig):
-    right = True
+    args = parser.parse_args()
 
-if(maxre <= maxc + wig):
-    left = True
+    if not os.path.isfile(args.srcpath):
+        print("Invalid path, exiting")
+        return
 
-if(maxce <= maxr + wig):
-    up = True
+    cap = cv2.VideoCapture(args.srcpath)
+    numframes = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-if(maxcs >= maxr - wig):
-    down = True
+    counter = -1
+    speedinator = 10
 
-p1 = (maxc, maxr)
+    thresh = 10
+    val = -30
 
-p2 = -1
+    allimages = []
+    first = True
+    total_image = 0
 
-if(up and right):
-    p2 = (width-1, 0)
+    debug = False
 
-if(up and left):
-    p2 = (0, 0)
+    while(cap.isOpened()):
 
-if(down and right):
-    p2 = (width-1, height-1)
+        counter += 1
 
-if(down and left):
-    p2 = (0, height-1)
+        #image = cv2.imread(sys.argv[1])
 
-if(p2 == -1):
-    print("No obvious box")
-    quit()
+        ret, image = cap.read()
 
-xl = min(p1[0], p2[0])
-xh = max(p1[0], p2[0])
-yl = min(p1[1], p2[1])
-yh = max(p1[1], p2[1])
 
-video = VideoFileClip(sys.argv[1])
-boxvideo = video.fx(crop, x1 = xl, y1 = yl, x2 = xh, y2 = yh)
+        # print("Frame " + str(counter) + "/" + str(numframes))
 
-boxvideo.write_videofile("boxvideo.mp4")
+        if(counter % speedinator != 0):
+            continue
 
-def drawrect(frame):
-    cv2.rectangle(frame, (xl, yl), (xh, yh), (0,0,0), -1)
-    return frame
+        if(image is None):
+            break
 
-outervideo = video.fl_image(drawrect)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-outervideo.write_videofile("outervideo.mp4")
+        laplacian = cv2.Laplacian(image, cv2.CV_8U)
+        laplacian = laplacian.astype(int)
+        laplacian[laplacian < thresh] = val
+
+        if(first):
+            total_image = laplacian
+            first = False
+        else:
+            total_image += laplacian
+
+
+        #ret, laplacian = cv2.threshold(laplacian,127,255,cv2.THRESH_BINARY)
+
+        allimages.append(laplacian)
+
+        #cv2.imshow('img', sobel_8u)
+        #cv2.waitKey(0)
+        #cv2.destroyAllWindows()
+
+    #print(total_image)
+
+    #av = np.mean(allimages, axis=0)
+    total_image = total_image.astype(float)
+    total_image /= counter
+
+    #print(total_image)
+
+    #print(allimages[0].shape)
+    #print(av.shape)
+    total_image = np.clip(total_image, 0, 255)
+
+    total_image *= (255 / total_image.max())
+
+    total_image = total_image.astype(np.uint8)
+
+    #print(total_image)
+
+    if(debug):
+        cv2.imshow('img', total_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    total_image = cv2.threshold(total_image, 20, 255, cv2.THRESH_BINARY)[1]
+    #edges = cv2.Canny(total_image,0,0)
+    edges = total_image
+
+    if(debug):
+        cv2.imshow('img', total_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    height, width = total_image.shape[:2]
+    linesize = int(min(width, height)/8)
+
+    padding = int(min(width, height)/8)
+
+
+
+    flat = np.sum(edges, axis = 0)
+    flat[0] = 0
+    s = flat.shape[0]
+    flat[s - 1] = 0
+    # print("Flat shape: " + str(s))
+    #print(flat)
+
+    #plt.plot(range(s), flat)
+    #plt.show()
+
+    # print("Maxcol: " + str(flat.argmax()) + "/" + str(s))
+    #print(flat)
+    #print("==========================================================================================================")
+
+    edges = edges.astype(int)
+    edges[edges == 0] = -255
+
+    maxc = -1
+    maxcs = -1
+    maxce = -1
+
+    maxr = -1
+    maxrs = -1
+    maxre = 0-1
+
+    for i in range(3, width - 3):
+        col = edges[:, i]
+        start, end = maxsubarray(col)
+
+        if(end - start > maxce - maxcs):
+            maxce = end
+            maxcs = start
+            maxc = i
+
+        #print("Col " + str(i) + "/" + str(width) + ": " + str(start) + " -> " + str(end))
+
+    for i in range(3, height - 3):
+        row = edges[i, :]
+        start, end = maxsubarray(row)
+
+        if(end - start > maxce - maxcs):
+            maxre = end
+            maxrs = start
+            maxr = i
+
+
+    if(maxce - maxcs < linesize or maxre - maxrs < linesize):
+        print("Did not detect picture in picture")
+        return
+
+    cap = cv2.VideoCapture(args.srcpath)
+    ret, image = cap.read()
+
+
+    cv2.line(image, (maxc, maxcs), (maxc, maxce), (0,0,255), 3, cv2.LINE_AA)
+    cv2.line(image, (maxrs, maxr), (maxre, maxr), (0,0,255), 3, cv2.LINE_AA)
+
+    if(debug):
+        cv2.imshow('img', image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    wig = 2 # wiggle room for lines to overlap
+
+    right = False
+    left = False
+    up = False
+    down = False
+
+    if(maxrs >= maxc - wig):
+        right = True
+
+    if(maxre <= maxc + wig):
+        left = True
+
+    if(maxce <= maxr + wig):
+        up = True
+
+    if(maxcs >= maxr - wig):
+        down = True
+
+    p1 = (maxc, maxr)
+
+    p2 = -1
+
+    if(up and right):
+        p2 = (width-1, 0)
+
+    if(up and left):
+        p2 = (0, 0)
+
+    if(down and right):
+        p2 = (width-1, height-1)
+
+    if(down and left):
+        p2 = (0, height-1)
+
+    if(p2 == -1):
+        print("No obvious box")
+        quit()
+
+    xl = min(p1[0], p2[0])
+    xh = max(p1[0], p2[0])
+    yl = min(p1[1], p2[1])
+    yh = max(p1[1], p2[1])
+
+    video = VideoFileClip(args.srcpath)
+    boxvideo = video.fx(crop, x1 = xl, y1 = yl, x2 = xh, y2 = yh)
+
+    boxvideo.write_videofile(box_path)
+
+    def drawrect(frame):
+        cv2.rectangle(frame, (xl, yl), (xh, yh), (0,0,0), -1)
+        return frame
+
+    outervideo = video.fl_image(drawrect)
+
+    outervideo.write_videofile(outer_path)
+
+
+if __name__ == "__main__":
+    main()
