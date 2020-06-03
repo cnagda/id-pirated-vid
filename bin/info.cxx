61c4c1c9429654e728b55830ea46c0b043eb8335
Emma Kristovich <emkristovich@gmail.com>
2020-06-03 09:13:48 -0700

    info deserves grey too

--- bin/info.cxx
+++ bin/info.cxx
@@ -15,8 +15,19 @@
 using namespace cv;
 using namespace std;
 
+#define WHITE       "\u001b[37m"
+#define BLUE        "\033[94m"
+#define GREEN       "\033[92m"
+#define YELLOW      "\033[93m"
+#define RED         "\033[91m"
+#define ENDC        "\033[0m"
+#define GREY        "\u001b[38;5;244m"
+#define BOLD        "\u001b[1m"
+#define UNDERLINE   "\u001b[4m"
+
 int main(int argc, char **argv)
 {
+
     if (argc < 2)
     {
         printf("usage: ./info dbPath\n");
@@ -27,19 +38,19 @@ int main(int argc, char **argv)
 
     auto config = db.getConfig();
 
-    std::cout << "\nDatabase Configuration:" << std::endl;
-    std::cout << std::left << std::setw(20) << "kFrame:" << config.KFrames << std::endl;
-    std::cout << std::left << std::setw(20) << "kScene:" << config.KScenes << std::endl;
-    std::cout << std::left << std::setw(20) << "thresholdScene:" << config.threshold << std::endl;
+    std::cout << BOLD << "\nDatabase Configuration:" << ENDC << std::endl;
+    std::cout << GREY << std::left << std::setw(20) << "kFrame:" << config.KFrames << ENDC << std::endl;
+    std::cout << GREY << std::left << std::setw(20) << "kScene:" << config.KScenes << ENDC << std::endl;
+    std::cout << GREY << std::left << std::setw(20) << "thresholdScene:" << config.threshold << ENDC << std::endl;
 
     auto vidList = db.listVideos();
 
-    std::cout << "\nVideo List:" << std::endl;
+    std::cout << BOLD << "\nVideo List:" << ENDC << std::endl;
     int count = 0;
+    std::cout << GREY;
     for (auto vid : vidList) {
         std::cout << std::left << std::setw(40) << vid << "\t";
         if (++count %2 == 0) {  std::cout << std::endl;  }
     }
-    std::cout << std::endl;
-
+    std::cout << ENDC << std::endl;
 }
